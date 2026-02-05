6927c0bd597ba690a52f2bb7e7996a1b0f2509e7
Shuo729 <150384601+Shuo729@users.noreply.github.com>
2026-02-05 04:20:35 -0800

    Setup Itinerary Tab

--- WeTravelApp/app/(tabs)/_layout.tsx
+++ WeTravelApp/app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="itinerary"
+        options={{
+          title: 'Itinerary',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
