f2749201a2111cd540b4c2a336cfdb3bf9b607a0
karlaAx21 <143290370+karlaAx21@users.noreply.github.com>
2026-02-17 15:33:40 -0800

    create data base
    create 2 .py files: tables.py create the tables and database, DBdelete.py delete the database completely so when test it the data does not accumulate

---
+++ Database/tables.py
@@ -0,0 +1,94 @@
+import sqlite3
+
+# Create or connect to a database file
+#connection = sqlite3.connect("databaseWT.db")
+
+# Create a cursor to run SQL commands
+#cursor = connection.cursor()
+
+def create_tables():
+
+    connection = sqlite3.connect("databaseWT.db")
+    cursor = connection.cursor()
+
+    #users table 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fist_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,          
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        phone_number TEXT,
+        image_url TEXT -- Image, might change 
+    )
+    """)
+    print("Users Table create")
+    #Posts table 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        content TEXT,
+        image_url TEXT, -- Image, might change 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """)
+    print("Posts Table create")
+
+    #Locations
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS locations (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER NOT NULL,
+        latitude REAL,
+        longitude REAL,
+        address TEXT,
+        city TEXT,
+        state TEXT,
+        country TEXT,
+        FOREIGN KEY (post_id) REFERENCES posts(id)
+    )
+    """)
+    print("Locations Table create")
+    
+    #Comments Table 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        comment_text TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (post_id) REFERENCES posts(id),
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """)
+    print("Comments Table create")
+
+    #Likes table 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS likes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (post_id) REFERENCES posts(id),
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        UNIQUE (post_id, user_id)
+    )
+    """)
+    print("Likes Table create")
+
+
+####
+
+try:
+    connection = sqlite3.connect("databaseWT.db")
+    print("Connected to database")
+    create_tables()
+except sqlite3.Error as error:
+    print("Failed to connect:", error)
+
