e2fb851b141cc59a397cc2d6bfb1ddc6a0338990
George Hanna <georgeah2005@gmail.com>
2026-03-10 10:33:03 -0700

    Add location coordinates data file mapping post locations to lat/lng

---
+++ WeTravelApp/data/location-coords.ts
@@ -0,0 +1,9 @@
+// Maps lowercase location strings to [latitude, longitude]
+export const LOCATION_COORDS: Record<string, [number, number]> = {
+  'peru':                  [-12.0464, -77.0428],
+  'jordan':                [30.3285,  35.4444],
+  'los angeles, ca':       [34.0522,  -118.2437],
+  'santorini, greece':     [36.3932,  25.4615],
+  'kyoto, japan':          [35.0116,  135.7681],
+  'serengeti, tanzania':   [-2.3333,  34.8333],
+};
